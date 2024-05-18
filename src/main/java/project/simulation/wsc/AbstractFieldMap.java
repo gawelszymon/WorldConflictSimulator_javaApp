4bb2754f9c7c2e9f28ce637806d1afa0e9b3d30d
gawelszymon <szymon.gawel.szg@gmail.com>
2024-05-18 19:08:51 +0200

    test2

--- src/main/java/project/simulation/wsc/AbstractFieldMap.java
+++ src/main/java/project/simulation/wsc/AbstractFieldMap.java
@@ -2,7 +2,7 @@ package project.simulation.wsc;
 
 import java.util.*;
 
-//abstract class is a kind of class that cannot be directly instance using word "new", the purpose of this class is to
+//the abstract class is a kind of class that cannot be directly instance using word "new", the purpose of this class is to
 //enable a pattern for classes inherited by it (methods and fields are in inherited classes)
 abstract public class AbstractFieldMap implements IFieldMap, IElementChangeObserver {
     protected final Map<Vector2D, MapSquare> elements;
